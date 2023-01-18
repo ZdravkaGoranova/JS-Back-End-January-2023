7a9973b02cd12393a09c47e64c5d7540cde91721
Zdravka Goranova <106737347+ZdravkaGoranova@users.noreply.github.com>
2023-01-19 00:02:09 +0200

    Add files via upload

---
+++ 3.ExpressJS and Templating/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const res = require('express/lib/response.js');
+const app = express();//инстанция на Express server
+
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send(`<h1>Hello from Exppress!</h1>
+    <a href ="/cats">Cats</a>${'\n'}
+    <a href ="/dogs">Dogs</a>
+    `)
+});
+
+app.post('/cats', (req, res) => {
+    res.send('Cats is received')
+});
+app.put('/cats', (req, res) => {
+    res.send('Cats is updated')
+});
+app.delete('/cats', (req, res) => {
+    res.send('Cats is deleted')
+});
+app.get('/cats', (req, res) => {
+    res.send('<h1>Cats page</h1>')
+});
+
+app.get('/cats/1', (req, res) => {
+    res.download('./cat.jpg');
+});
+app.get('/cats/:catId', (req, res) => {
+    console.log(req.params);
+    res.send(`Individual Cat page on cat is catId: ${req.params.catId}`);
+    //req.params.(catId)- трябва да се казва както е наречено след : (catId)
+});
+
+app.get('/dogs', (req, res) => {
+    res.send('<h1>Dogs page</h1>')
+});
+
+app.get('/json', (req, res) => {
+    res.json({ ok: true, message: 'Hello from JSON' })
+})
+
+app.get('/redirect', (req,res) => {
+    res.redirect('/rediredtedddd');
+})
+
+app.get('/rediredtedddd', (req, res) => {
+    res.send('This is rediredtedddd page :)')
+})
+
+
+app.get('*', (req, res) => {//Ако е GET заявка означава  *- всички пътища
+    res.send(`404`)
+});
+
+app.listen(5000, () => console.log('Server is listening on port 5000...'));
